76b98b6ca34bd56f5c8cf51a7f9790eb29d2cd56
Angelica Beltran <angelica.beltran11@gmail.com>
2023-05-08 14:35:20 -0500

    added the rows from blocks from color block assignment

--- script.js
+++ script.js
@@ -1,28 +1,47 @@
 $(document).ready(onReady);
-
+//when this button is clicked ...
 function onReady() {
-    console.log("I got this.");
-   
-    $("#generate-btn").on("click", generateRow);
+  console.log("I got this.");
+
+  $("#generate-btn").on("click", generateRow);
+} //end of onReady
+
+//When this function is called i want it to do...
+function generateRow(event) {
+  const rowOneColor = $("#rowOne-btn");
+//   const rowTwoColor = $("#rowTwoColor");
+//   const rowThreeColor = $("#rowThreeColor");
+//   const rowFourColor = $("#rowFourColor");
+
+  event.preventDefault();
+} //end of function generate row
+ //---------------------------------------------------------------
 
 
 
+ 
+$("#rowOneColor").on("click", rowOneColorAppend);
+$("#rowTwoColor").on("click", rowTwoColorAppend);
+$("#rowThreeColor").on("click", rowThreeColorAppend);
+$("#rowFourColor").on("click", rowFourColorAppend);
 
-  } //end of onReady
-
-
-  function generateRow(event) {
-    const redButton = $("#red-btn");
-    const blueButton = $("#blue-btn");
-    const greenButton = $("#green-btn");
-    const yellowButton = $("#yellow-btn");
-
-
-
-
-    
-    console.log(inputFirstName, inputLastName, inputID, inputTitle, inputSalary);
-    //---------------------------------------------------------------
-    event.preventDefault();
-
-  }//end of function generate row 
+function rowOneColorAppend() {
+  $("#rowOneColor").append('<div class="block red-fill"></div>');
+  // red++;
+  // $('#red-count').append(red);
+}
+function rowTwoColorAppend() {
+  $("#rowTwoColor").append('<div class="block blue-fill"></div>');
+  // blue++;
+  // blueCount();
+}
+function rowThreeColorAppend() {
+  $("#rowThreeColor").append('<div class="block green-fill"></div>');
+  // green++;
+  // greenCount();
+}
+function rowFourColorAppend() {
+  $("#rowFourColor").append('<div class="block yellow-fill"></div>');
+  //     yellow++;
+  //     yellowCount();
+}
