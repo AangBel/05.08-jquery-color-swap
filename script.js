4caf6cbb573c5905d45e741d273f0d154fd0e340
Angelica Beltran <angelica.beltran11@gmail.com>
2023-05-08 14:07:53 -0500

    added html and css for rows

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 $(document).ready(onReady);
+
+function onReady() {
+    console.log("I got this.");
+   
+    $("#generate-btn").on("click", generateRow);
+
+
+
+
+  } //end of onReady
+
+
+  function generateRow(event) {
+    const redButton = $("#red-btn");
+    const blueButton = $("#blue-btn");
+    const greenButton = $("#green-btn");
+    const yellowButton = $("#yellow-btn");
+
+
+
+
+    
+    console.log(inputFirstName, inputLastName, inputID, inputTitle, inputSalary);
+    //---------------------------------------------------------------
+    event.preventDefault();
+
+  }//end of function generate row 
